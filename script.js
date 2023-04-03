7ed7d2f825f3bc56b74a1f2d1c4ce81cd0c2917a
merv-e <34655454+merv-e@users.noreply.github.com>
2023-04-03 13:47:39 +0200

    updates on my previous portfolio - new projects (2 of them) are added. - changes are made in the UX

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const navigateToGithubPage = () => {
+    navigate(https://github.com/merv-e?tab=repositories)
+}
+// button onclick 
+
+// https://github.com/merv-e?tab=repositories
