2fb55e18df97e25c8bc9ddc9eaaca0609d772ecc
merv-e <34655454+merv-e@users.noreply.github.com>
2023-04-13 16:09:23 +0200

    - some TODO comments are added. - JS file deleted since it's not being used at the moment.

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const navigateToGithubPage = () => {
-    navigate(https://github.com/merv-e?tab=repositories)
-}
-// button onclick 
-
-// https://github.com/merv-e?tab=repositories
